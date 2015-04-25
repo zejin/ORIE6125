2b6b3d5fc95444cf8785ca7c49ff798b88014075
skyler120 <skyler@mit.edu>
2015-04-25 15:19:03 -0400

    fixed syntactical bugs

--- est.py
+++ est.py
@@ -60,29 +60,29 @@ def BS_testing(X1,X2,C=1.5):
     sigma_hat = (np.dot((X1.T - X1_bar).T, X1.T - X1_bar) +  np.dot((X2.T - X2_bar).T, X2.T - X2_bar))/ float(n) # O(np^2)
     tr_sigma_hat = sum(np.diag(sigma_hat)) # O(p)
 
-    M_stat = np.dot(X1_bar-X2_bar, X1_bar, X2_bar) - n*tr_sigma_hat/n1/n2
+    M_stat = np.dot((X1_bar-X2_bar).T, X1_bar-X2_bar) - n*tr_sigma_hat/n1/n2
 
-    B_square = n^2/(n+2)/(n-1) * ((la.norm(sigma_hat,type= "fro")^2-1)/n * (tr_sigma_hat)^2)
+    B_square = float(n)**2/(n+2)/(n-1) * ((la.norm(sigma_hat, 'fro')**2-1)/n * (tr_sigma_hat)**2)
 
-    var_M_Original = 2*n*(n+1)/(n1*n2)^2 * B_square
+    var_M_Original = 2*n*(n+1)/(n1*n2)**2 * B_square
 
-    Z_original = float(M_stat/sqrt(var_M_Original))
+    Z_original = float(M_stat/np.sqrt(var_M_Original))
 
 
-    tau = C*sqrt(np.log(p)/n)
+    tau = C*np.sqrt(np.log(p1)/n)
 
-    sigma_hat_tau = (np.dot(X1,X1) + np.dot(X2,X2))/n
+    sigma_hat_tau = (np.dot(X1,X1.T) + np.dot(X2,X2.T))/n
 
     sigma_ij_hat_tau = sigma_hat_tau[abs(sigma_hat_tau) > tau]
-    Q_hat <- sum(sigma_ij_hat_tau * sigma_ij_hat_tau)
+    Q_hat = sum(sigma_ij_hat_tau * sigma_ij_hat_tau)
 
-    var_M_Modified = 2*n*(n+1)/(n1*n2)^2 * Q_hat
+    var_M_Modified = 2*n*(n+1)/(n1*n2)**2 * Q_hat
 
-    Z_modified = float(M_statsqrt(var_M_Modified))
+    Z_modified = float(M_stat/np.sqrt(var_M_Modified))
   
     return (2*(1-np.random.normal(abs(Z_original)))), 2*(1-np.random.normal(abs(Z_modified)))
 
-def CQ_testing(X1,C2,C=1.5):
+def CQ_testing(X1,X2,C=1.5):
     p1,n1 = X1.shape
     p2,n2 = X2.shape
     n = n1+n2
@@ -104,25 +104,25 @@ def CQ_testing(X1,C2,C=1.5):
     for j in range(n1):
         for k in range(n1):
             if j!=k:
-                X1_bar_jk = np.mean(X1[:][j:k],axis=1)
-                s1 = s1+ np.dot(np.dot(X1[:][j].T,(X1[:][k]-X1_bar_jk)),np.dot(X1[:][k].T,(X1[:][j]-X1_bar_jk)))
+                X1_bar_jk = np.mean(X1[:,j:k],axis=1)
+                s1 = s1+ np.dot(np.dot(X1[:,j].T,(X1[:,k]-X1_bar_jk)),np.dot(X1[:,k].T,(X1[:,j]-X1_bar_jk)))
     sigma1_F_norm_square = s1/n1/(n1-1)
 
     s2 = 0
     for j in range(n2):
         for k in range(n2):
             if j!=k:
-                X2_bar_jk = np.mean(X2[:][j:k],axis=1)
-                s2 = s2+ np.dot(np.dot(X2[:][j].T,(X2[:][k]-X2_bar_jk)),np.dot(X2[:][k].T,(X2[:][j]-X2_bar_jk)))
+                X2_bar_jk = np.mean(X2[:,j:k],axis=1)
+                s2 = s2+ np.dot(np.dot(X2[:,j].T,(X2[:,k]-X2_bar_jk)),np.dot(X2[:,k].T,(X2[:,j]-X2_bar_jk)))
     sigma2_F_norm_square = s2/n2/(n2-1)
 
     sigma12_innerprod = np.sum(np.diag(np.dot(np.dot((X1-X1_bar),X1.T),np.dot((X2-X2_bar),X2.T))))/(n1-1)/(n2-1)
 
     var_T_original = sigma1_F_norm_square*2/n1/(n1-1)+sigma2_F_norm_square*2/n2/(n2-1)+sigma12_innerprod*4/n1/n2
 
-    Z_original = float(T_stat/sqrt(var_T_original))
+    Z_original = float(T_stat/np.sqrt(var_T_original))
 
-    tau = C*sqrt(np.log(p)/n)
+    tau = C*np.sqrt(np.log(p1)/n)
     sigma1_hat_tau = np.dot(X1,X1.T)/n1
 
     sigma1_ij_hat_tau = sigma1_hat_tau[abs(sigma1_hat_tau) > tau]
@@ -139,10 +139,11 @@ def CQ_testing(X1,C2,C=1.5):
     Q12_hat = np.sum(sigma12_ij_hat_tau * sigma12_ij_hat_tau)
 
     var_T_modified = Q1_hat * 2 / n1 / (n1 - 1) + Q2_hat * 2 / n2 / (n2 - 1) + Q12_hat * 4 / n1 / n2
-    Z_modified = float(T_stat/sqrt(var_T_modified))
+    Z_modified = float(T_stat/np.sqrt(var_T_modified))
 
     return 2*(1-np.random.normal(abs(Z_original))), 2*(1-np.random.normal(abs(Z_modified)))
 
 
+print CQ_testing(np.random.randn(2, 2), np.random.randn(2, 2))
 
 
